eb49f06b5e8c20303fa5bd446a12aef011a14170
Wang Chenguang <chenguang.wang@tendcloud.com>
2023-03-30 17:57:09 +0800

    fix: 默认访问聊天页

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: '/home',
+      redirect: '/chat',
       name: 'Navigation',
       component: Navigation,
       children: [
